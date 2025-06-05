b84e9b1b5972e77badf2f38c478325819ff393ee
NihadRasulzada <nihadrs2005@gmail.com>
2025-06-06 00:59:08 +0400

    Init

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;  
+using System.Text;  
+
+internal class Program 
+{
+    private static async Task Main(string[] args)  
+    {
+        ConnectionFactory factory = new ConnectionFactory();  // RabbitMQ serverinə qoşulma üçün fabrik sinifi
+        factory.Uri = new Uri("amqps://jqtpztoz:EGeJb2LSQrMdWnmPeSJveypSJNNqkl3j@duck.lmq.cloudamqp.com/jqtpztoz");  // RabbitMQ serverinin URI-sini təyin edir
+
+        using (IConnection connection = await factory.CreateConnectionAsync())  // RabbitMQ serverinə asinxron qoşulma
+        {
+            IChannel channel = await connection.CreateChannelAsync();  // Bağlantıdan bir kanal (channel) yaradır
+            await channel.QueueDeclareAsync(queue: "hello-queue",  // "hello-queue" adlı bir sıra (queue) elan edir
+                                            durable: true,  // Sıra davamlı olacaq, yəni server restart olarsa, məlumatlar itməyəcək
+                                            exclusive: false,  // Bu sıra yalnız bu bağlantı üçün məhdud olmayacaq
+                                            autoDelete: false);  // Sıra heç bir istehsaldan sonra avtomatik olaraq silinməyəcək
+
+            var basicProperties = new BasicProperties();  // Mesajın əsas xüsusiyyətlərini saxlamaq üçün obyekt
+
+            // Aşağıda şərh edilmiş hissə, mesajın davamlı olmasını təmin etmək üçün istifadə edilə bilərdi
+            //basicProperties.Persistent = true;  // Mesajın davamlı olması üçün (server restart olarsa da məlumat itməsin)
+
+            
+            Enumerable.Range(1, 50).ToList().ForEach(async x =>  
+            {
+                string message = $"Message {x}";  // Hər bir mesajın məzmununu təyin edir
+                byte[] body = Encoding.UTF8.GetBytes(message);  // Mesajı UTF-8 ilə byte dizisinə çevirir
+
+                await channel.BasicPublishAsync(  // Mesajı sıraya göndərir
+                    exchange: string.Empty,  // Məlumatı birbaşa qeydiyyatdan keçirir (default exchange istifadə edilir)
+                    routingKey: "hello-queue",  // Mesajın gedəcəyi sıra
+                    mandatory: false,  // Məcburi göndərmə deyil, əgər hədəf mövcud deyilsə, mesaj itə bilər
+                    basicProperties: basicProperties,  // Mesajın xüsusiyyətləri (məsələn, davamlılıq)
+                    body: body);  // Mesajın məzmunu
+
+                Console.WriteLine($" [x] Sent {message}");  
+            });
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        ConnectionFactory factory = new ConnectionFactory();  // RabbitMQ serverinə qoşulma üçün fabrik sinifi
+        factory.Uri = new Uri("amqps://jqtpztoz:EGeJb2LSQrMdWnmPeSJveypSJNNqkl3j@duck.lmq.cloudamqp.com/jqtpztoz");  // RabbitMQ serverinin URI-sini təyin edir
+
+        using (IConnection connection = await factory.CreateConnectionAsync())  // RabbitMQ serverinə asinxron qoşulma
+        {
+            IChannel channel = await connection.CreateChannelAsync();  // Bağlantıdan bir kanal (channel) yaradır
+
+            // Prefetch parametrlərini təyin edir ki, hər bir istehsalçıya ən çox 5 mesaj göndərilsin
+            await channel.BasicQosAsync(prefetchSize: 0,  // Mesajların ölçüsünü təyin edir, burada 0 - ölçü məhdudiyyəti yoxdur
+                                        prefetchCount: 5,  // Hər bir istehsalçıya göndəriləcək maksimal mesaj sayı
+                                        global: false);  // Global QoS tətbiq etməmək, yalnız bu kanalda tətbiq edilir
+
+            var consumer = new AsyncEventingBasicConsumer(channel);  // Asinxron bir istehlakçı (consumer) obyekti yaradılır
+
+            // "hello-queue" adlandırılan sıradan mesajları istehlak etmək üçün bu metod istifadə edilir
+            await channel.BasicConsumeAsync(queue: "hello-queue",  // Hedef sıra adı
+                                            autoAck: false,  // Mesajın avtomatik olaraq təsdiq edilməməsi
+                                            consumer: consumer);  // İstehlakçı obyektini təyin edir
+
+            // Mesaj alındıqda bu hadisə işləyir
+            consumer.ReceivedAsync += async (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());  // Mesajın bədənini UTF-8 ilə oxuyur
+                Console.WriteLine($" [x] Received {message}"); 
+
+                // Mesaj alındıqdan sonra təsdiq edilir (acknowledgement)
+                await channel.BasicAckAsync(e.DeliveryTag, true);  // Mesajın alınması təsdiq edilir
+            };
+
+            Console.ReadLine();  
+        }
+    }
+}
